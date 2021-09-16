1dce2781aa29b68368769317825debe5152f0d13
Bruno Rodrigo <brunnogost2017@gmail.com>
2021-09-16 16:58:33 -0300

    atividades js

---
+++ Atividade 1/exercicio.js
@@ -0,0 +1,32 @@
+//questão 1 - var tipo number
+var number = 19
+console.log(number)
+
+//questão 2 - var tipo string
+var texto = "Aqui está o texto"
+console.log(texto)
+
+//questao 3 - var tipo boolean
+var verdadeVei = true 
+var mentiraVei = false
+console.log("Clau vai nos mandar um pix? " + verdadeVei)
+console.log("E vai nos levar para as Maldivas? " + mentiraVei)
+
+//questao 4 - var tipo array 
+var top3Cinemas = ["São Luiz", "Cinépolis", "CineSystem"]
+console.log(top3Cinemas)
+
+//questao 5 - var tipo Object
+var receitaDeMiojo = {passo1: "Ferver a água", passo2: "Pôr o miojão na água fervente", passo3: "Pôr o temperinho", passo4: "Desligar a panela e encher o bucho"}
+console.log(receitaDeMiojo.passo1)
+console.log(receitaDeMiojo.passo2)
+console.log(receitaDeMiojo.passo3)
+console.log(receitaDeMiojo.passo4)
+
+//questao 6 - var tipo Undefined
+var valorNaoDefinido
+console.log("Não há valor definido: " + valorNaoDefinido)
+
+//questao 7 - var tipo Null
+var valorVazio = null
+console.log("Não há valor: " + valorVazio)
---
+++ Atividade 2/exercicio.js
@@ -0,0 +1,11 @@
+//questao 1 - Criar duas variáveis usando a palavra-chave var.
+var numero = 18
+var texto = "olha só, uma variável tipo 'var', ou seja, ela é global."
+
+//questao 2 - Criar duas variáveis usando a palavra-chave let.
+let number = 19
+let text = "olha lá, esta variável é do tipo 'let', ou seja, ela é uma variável local." 
+
+//questao 3 - Criar duas constantes usando a palavra-chave const.
+const nummer = 20
+const geschrieben = "olha só, bonito, uma variável do tipo 'const', ou seja, ela é global e imutável."
